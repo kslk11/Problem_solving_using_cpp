460c01977809c7fce1174afbff1b28e7b2223554
Kaushal Kumar <104885742+kslk11@users.noreply.github.com>
2025-05-28 10:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.68%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
-        for(auto it : nums){
+        for(int it : nums){
             result ^=it;
         }
     return result;
