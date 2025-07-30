b408a5b656fc859e03c3c8d2f148a49a5f72f353
Kaushal Kumar <104885742+kslk11@users.noreply.github.com>
2025-07-30 21:52:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.47%) - LeetSync

---
+++ 504-base-7/base-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+       if(num ==0) return "0";
+
+       bool isNegative =num<0;
+
+       num =abs(num);
+       string result="";
+       while(num>0){
+        result =char('0'+num%7)+result;
+        num/=7;
+       } 
+       return isNegative ? "-"+result:result;
+    }
+};
